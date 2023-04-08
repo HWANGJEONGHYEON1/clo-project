a4b1c31b0ff6d9d139d36a9c310dc73216bc2d1a
황정현 <jhhwang@wemakeprice.com>
2023-04-09 01:31:06 +0900

    feat project 구축

---
+++ src/main/java/com/example/cloproject/CloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloproject/CloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
