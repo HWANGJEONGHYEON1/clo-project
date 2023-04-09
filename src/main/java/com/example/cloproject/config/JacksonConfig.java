4ab088d0e22401d44173cece856b072668fca347
황정현 <jhhwang@wemakeprice.com>
2023-04-10 00:23:06 +0900

    feat jackson config add

--- src/main/java/com/example/cloproject/config/JacksonConfig.java
+++ src/main/java/com/example/cloproject/config/JacksonConfig.java
@@ -1,2 +1,30 @@
-package com.example.cloproject.config;public class JacksonConfig {
+package com.example.cloproject.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    /*
+    역직렬화 할 때 LocalDate 값을 제대로 읽지 못하여 추가
+     */
+    private final JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+    @Bean
+    public CsvMapper csvMapper() {
+        final CsvMapper csvMapper = new CsvMapper();
+        csvMapper.registerModule(javaTimeModule);
+        return csvMapper;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
 }
--- src/main/java/com/example/cloproject/config/LocalDateDeserializer.java
+++ src/main/java/com/example/cloproject/config/LocalDateDeserializer.java
@@ -1,2 +1,38 @@
-package com.example.cloproject.config;public class LocalDateDeserializer {
+package com.example.cloproject.config;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * 요청 Date 형식이 달라 두 가지 형식을 다 수용할 수 있도록 추가
+ */
+@Slf4j
+public class LocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+    private static final DateTimeFormatter[] FORMATTERS = {
+            DateTimeFormatter.ofPattern("yyyy.MM.dd"),
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    };
+
+    @Override
+    public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        String dateStr = p.getValueAsString();
+        for (DateTimeFormatter formatter : FORMATTERS) {
+            try {
+                return LocalDate.parse(dateStr, formatter);
+            } catch (DateTimeParseException e) {
+                log.warn("formatter {}", formatter);
+            }
+        }
+        throw new JsonParseException(p, "Failed to parse LocalDate: " + dateStr);
+    }
 }
