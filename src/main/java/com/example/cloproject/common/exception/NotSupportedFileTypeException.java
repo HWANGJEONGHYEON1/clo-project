dd85d3096c650ba665b8ce3d945658b0aeb9ce0d
황정현 <jhhwang@wemakeprice.com>
2023-04-10 00:22:38 +0900

    feat exception add

--- src/main/java/com/example/cloproject/common/exception/NotSupportedFileTypeException.java
+++ src/main/java/com/example/cloproject/common/exception/NotSupportedFileTypeException.java
@@ -1,2 +1,23 @@
-package com.example.cloproject.common.exception;public class NotSupportedFileTypeException {
+package com.example.cloproject.common.exception;
+
+public class NotSupportedFileTypeException extends RuntimeException {
+    public NotSupportedFileTypeException() {
+        super();
+    }
+
+    public NotSupportedFileTypeException(String message) {
+        super(message);
+    }
+
+    public NotSupportedFileTypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotSupportedFileTypeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotSupportedFileTypeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
