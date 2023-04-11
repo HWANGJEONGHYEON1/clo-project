cb5cc289b9bd9aba473817171c9d4825ea88909a
jhhwnag <jhhwang@wemakeprice.com>
2023-04-11 12:50:41 +0900

    refactor branch on request content-type

--- src/main/java/com/example/cloproject/employee/controller/EmployeeController.java
+++ src/main/java/com/example/cloproject/employee/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ServerWebExchange;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
@@ -19,21 +21,27 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @RequestMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<String> addEmployeesFromJson(@RequestBody List<EmployeeCreateDto> body) throws IOException {
+        employeeService.addEmployeesFromRequestBody(body);
+        return new ResponseEntity<>("File uploaded successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping(consumes = {MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<String> addEmployeesFromCsv(@RequestBody String body) throws IOException {
+        employeeService.addEmployeesFromCsvRequestBody(body);
+        return new ResponseEntity<>("File uploaded successfully", HttpStatus.CREATED);
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> addEmployees(MultipartFile file) throws IOException {
         employeeService.addEmployeesFromFile(file);
         return new ResponseEntity<>("File uploaded successfully", HttpStatus.CREATED);
     }
 
-    @PostMapping(consumes = "text/csv")
-    public ResponseEntity<String> addEmployeesFromCsvBody(@RequestBody String body) throws IOException {
-        employeeService.addEmployeesFromCsvRequestBody(body);
-        return new ResponseEntity<>("success", HttpStatus.CREATED);
-    }
-
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> addEmployeesFromJsonBody(@RequestBody List<EmployeeCreateDto> employeeDtos) {
-        employeeService.addEmployeesFromRequestBody(employeeDtos);
-        return ResponseEntity.ok("File uploaded successfully");
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<String> addEmployeesFromFormData(String a) throws IOException {
+        employeeService.addEmployeesFromFormData(a);
+        return new ResponseEntity<>("File uploaded successfully", HttpStatus.CREATED);
     }
 }
